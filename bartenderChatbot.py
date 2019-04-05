356eebf66dba18680ecc6e050f22176ffffceff4
Raphael Chevallier <raphaelchevallier@hotmail.com>
2019-04-04 18:23:04 -0700

    changes and use of luis engine for the chatbot

--- bartenderChatbot.py
+++ bartenderChatbot.py
@@ -1,236 +1,119 @@
 from flask import Flask
 from flask import request
-from textblob import TextBlob
 import random
 import json
 import os
+import requests
 
 os.environ['NLTK_DATA'] = '~/nltk_data'
-GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up", "hi there", "good evening", "wuzzup my homie")
 GREETING_RESPONSES = ["What can I get you?", "Hello stranger, what can I serve up for you?", "Need a drink?", "Hi, hope you're thirsty. What can I get you?"]
-GOODBYE_KEYWORDS = ("bye", "good bye", "goodbye", "see ya")
 GOODBYE_RESPONSES = ["Have a good night", "Drive safe!", "Until next time"]
+DRINK_RESPONSES = ["Sure coming right up", "Here you go. Here's your drink", "Your drink is coming right up"]
+FOOD_RESPONSES = ["Sure coming right up", "Here you go. Here's your food", "Your food is coming right up"]
+STATUS_RESPONSES = ["I'm doing pretty good. It's a busy night tonight though. Are you going to order?", "Great! I'm doing fantastic", "Just trying to pay the bills man", "You going to have to tip for me to talk to ya"]
+WEATHER_RESPONSES = ["It's sunny today. Maybe you should go outside", "Pouring rain and freezing cold. Maybe you should stay in and get more drinks?", "It's hot. Sticky 85 degrees."]
+TRAVEL_RESPONSES = ["Go downtown. It's a great place to chill and get ice cream", "The lake is amazing. Great for the hot weather and swimming", "Big white is nearby. You should check that out."]
 HEDGE_RESPONSES = ["I have no idea what you're asking", "I'm not sure", "Can you re-phrase that?", "Pardon?", "Sorry I can't do that", "I'm confused"]
-DRINKS = ("vodka", "beer", "whiskey", "wine", "sex on the beach", "screwdriver", "green fairy", "whiskey", "absinthe", "acapulco gold", "amaretto", "bacardi", "baileys" "budweiser", "champagne", "daiquiri", "goldschlager" "guinness", "grey goose", "hootch", "jack daniels", "jagermeister", "limoncello", "mezcal", "moonshine", "pina colada", "tequila", "vodka", "zinfandel", "raki", "long island iced tea"  )
-DRINKSTYLE = ("on the rocks", "neat")#ask this after they request a drink
-YES_KEYWORDS = ("yes", "yeah", "certainly", "true", "yep", "yea", "okay", "exactly", "gladly")
-YES_RESPONSES = ["Okay", "Sounds good", "I like it", "Thats great!", "Exciting!"]
-NO_KEYWORDS = ("no", "nah", "negative", "not really", "never", "false", "nope")
-NO_RESPONSES = ["That's too bad", "Suit yourself", "If you say so", "I insist"]
 chat_log = {}
 
+  #Call on LUIS app here
+def LUIS_Call(message):
+	headers = {
+		# Request headers
+		'Ocp-Apim-Subscription-Key': 'b0a33f5fa946453ca4aa2a6ef8703568',
+	}
+
+	params = {
+		'q': message,
+		'verbose': 'true'
+	}
+	try:
+		#c35e79c6427144fc9695a914ce1874c6
+		r = requests.get('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/79b9c6d3-8740-4b03-97fe-93a83779505b?spellCheck=true&bing-spell-check-subscription-key=%7Bc35e79c6427144fc9695a914ce1874c6',headers=headers, params=params)
+		return r.json()
+
+	except Exception as e:
+		# pylint: disable=no-member
+		print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
 def createMessage(input):
-    '''Takes json facebook input and creates the message to return to facebook'''
-    input_msg = TextBlob(input['text'])
-    senderId = input['sid']
-    data = buildMessage(input_msg, senderId) 
-    #print(chat_log)
-    return str(data)  #this will return the wanted message back out to messenger
+	'''Takes json facebook input and creates the message to return to facebook'''
+	input_msg = input['text']
+	senderId = input['sid']
+	data = buildMessage(input_msg, senderId)
+	return str(data)  #this will return the wanted message back out to messenger
 
 
 def buildMessage(input_msg, senderId):
-    '''Core Logic to build the message.
-    If unsure how to reply, will respond with a hedge'''
+	'''Core Logic to build the message.
+	If unsure how to reply, will respond with a hedge'''
 
-    # Clears the user session to start over new
-    if input_msg.lower() == 'clear':
-        clearSession(senderId)
-        return "Session cleared"
+	# Clears the user session to start over new
+	if input_msg.lower() == 'clear':
+		clearSession(senderId)
+		return "Session cleared"
 
-    times_con = howManyMessages(senderId)
-    # If the user is greeting, respond with a greeting
-    if CheckForGreeting(input_msg):
-        return random.choice(GREETING_RESPONSES)
+	times_con = howManyMessages(senderId)
+	luis = LUIS_Call(input_msg)
+	intent = luis['topScoringIntent']['intent']
+	sentiment = luis['sentimentAnalysis']
+	print(sentiment)
+	if intent == 'getDrink' or intent == 'getFood':
+		print(luis['entities'])
 
-    # If the user is greeting, respond with a greeting
-    if CheckForGoodbye(input_msg):
-        # They're leaving, so clear the session
-        clearSession(senderId)
-        return random.choice(GOODBYE_RESPONSES)
 
-    # Break the message into parts
-    pronoun, noun, adjective, verb = getSpeechParts(input_msg)
-    num_drinks = chat_log[senderId]['drinks_served']
+	# If the user is greeting, respond with a greeting
+	if intent == 'greeting':
+		return random.choice(GREETING_RESPONSES)
 
-    # Search for a drink in the user input and respond as well as we can
-    drink = noun
-    if noun not in DRINKS:
-        drink = searchForDrink(input_msg)
-    if len(input_msg.words) == 1:
-        drink = input_msg
-    if drink in DRINKS:
-        if num_drinks > 3:
-            return "You are too drunk I am unable to serve you any more drinks. You can type 'clear' to tell me that you're sober again"
-        #increment drink counter
-        chat_log[senderId]['drinks_served'] = num_drinks + 1
-        if num_drinks <= 1:
-            return "One {0} coming right up!".format(drink)
-        if num_drinks == 2:
-            return "{0} for you, enjoy.".format(drink)
-        else:
-            return "Here is your {0}! Wow you've already had {1} drinks!".format(drink, num_drinks)
+	if intent == 'goodbye':
+		# They're leaving, so clear the session
+		clearSession(senderId)
+		return random.choice(GOODBYE_RESPONSES)
 
-    # Look for yes or no responses and respond with a weak hedge
-    if checkForYes(input_msg):
-        return random.choice(YES_RESPONSES)
-    if checkForNo(input_msg):
-        return random.choice(NO_RESPONSES)
+	if intent == 'getStatus':
+		return random.choice(STATUS_RESPONSES)
 
-    # If someone doesn't want anything
-    if noun == "nothing":
-        return "There isn't anything I can get for you? I am a master bar tender. You won't find any better."
+	if intent == 'getWeather':
+		return random.choice(WEATHER_RESPONSES)
 
-    #If we have a noun but no drink, we don't know what they want, so we answer with a question
-    if noun:
-        resp = []
-        if pronoun:
-            print("pron" + pronoun)
-            resp.append(pronoun)
-        if verb:
-            v = verb[0]
-            if v is not None:
-                resp.append(v)
-        if startsWithVowel(noun):
-            noun_pronoun = "an"
-        else:
-            noun_pronoun = "a"
-        resp.append(noun_pronoun + " " + noun + "?")
-        print(resp)
-        return " ".join(resp)
-    #If nothing caught, return a hedge
-    return random.choice(HEDGE_RESPONSES)
+	if intent == 'getTravel':
+		return random.choice(TRAVEL_RESPONSES)
 
+	if intent == 'getDrink':
+		num_drinks = chat_log[senderId]['drinks_served']
+		if num_drinks > 3:
+			return "Sorry you've had too many drinks. You're drunk rn"
+		else :
+			return random.choice(DRINK_RESPONSES)
+
+	if intent == 'getFood':
+		return random.choice(FOOD_RESPONSES)
+
+	return random.choice(HEDGE_RESPONSES)
 
 def createChatLog(senderId):
-    '''Keeps track of each session ID in a dictionary'''
-    log = {'times_contacted': 1, 'context': None, 'drinks_served': 0}
-    chat_log[senderId] = log
+	'''Keeps track of each session ID in a dictionary'''
+	log = {'times_contacted': 1, 'context': None, 'drinks_served': 0}
+	chat_log[senderId] = log
 
 
 def howManyMessages(senderId):
-    '''Check the chat log for number of messages and increment accordinly.'''
-    if senderId not in chat_log:
-        createChatLog(senderId)
-        #return value is number of messages received
-        return 1
-    else:
-        times_con = chat_log[senderId]['times_contacted'] + 1
-        chat_log[senderId]['times_contacted'] = times_con
-        return times_con
+	'''Check the chat log for number of messages and increment accordinly.'''
+	if senderId not in chat_log:
+		createChatLog(senderId)
+		#return value is number of messages received
+		return 1
+	else:
+		times_con = chat_log[senderId]['times_contacted'] + 1
+		chat_log[senderId]['times_contacted'] = times_con
+		return times_con
 
 
 def clearSession(senderId):
-    '''Delete any data from the user session log'''
-    chat_log[senderId]['times_contacted'] = 0
-    chat_log[senderId]['drinks_served'] = 0
-
-
-def CheckForGreeting(sentence):
-    '''Return boolean if the user sentence contains a greeting'''
-    for word in sentence.words:
-        if word.lower() in GREETING_KEYWORDS:
-            return True
-    return False
-
-
-def CheckForGoodbye(sentence):
-    '''Return boolean if the user wants to leave/end the sesion'''
-    for word in sentence.words:
-        if word.lower() in GOODBYE_KEYWORDS:
-            return True
-    return False
-
-
-def checkForYes(sentence):
-    '''Return boolean if the user indicated a yes/true reply'''
-    for word in sentence.words:
-        if word.lower() in YES_KEYWORDS:
-            return True
-    return False
-
-
-def checkForNo(sentence):
-    '''Return boolean if the user indicated a no/false reply'''
-    for word in sentence.words:
-        if word.lower() in NO_KEYWORDS:
-            return True
-    return False
-
-
-def searchForDrink(sentence):
-    '''Return boolean if the user sentence contains a drink'''
-    for word in sentence.words:
-        if word.lower() in DRINKS:
-            return word
-    return None
-
-
-def getSpeechParts(input_msg):
-    '''Use natural language processing to find and categorize each part of the sentance'''
-    pronoun = None
-    noun = None
-    adjective = None
-    verb = None
-    for s in input_msg.sentences:
-        pronoun = findPronoun(s)
-        noun = findNoun(s)
-        adjective = findAdjective(s)
-        verb = findVerb(s)
-    return pronoun, noun, adjective, verb
-
-
-def findPronoun(sent):
-    '''Return pronoun (I or You). Determins if the user is talking about
-    themselves or the bot.
-    Pronoun is represented as PRP in NLTK'''
-    pronoun = None
-    for word, part_of_speech in sent.pos_tags:
-        if part_of_speech == 'PRP' and word.lower() == 'you':
-            pronoun = 'I'
-        elif part_of_speech == 'PRP' and word == 'I' or word == 'i':
-            pronoun = 'You'
-    return pronoun
-
-
-def findVerb(sent):
-    '''Return verb represended as VB in NLTK'''
-    verb = None
-    pos = None
-    for word, part_of_speech in sent.pos_tags:
-        if part_of_speech.startswith('VB'):  # This is a verb
-            verb = word
-            pos = part_of_speech
-            break
-    return verb, pos
-
-
-def findNoun(sent):
-    '''Return noun represended as NN in NLTK'''
-    noun = None
-    if not noun:
-        for w, p in sent.pos_tags:
-            if p == 'NN' and w != 'i':  # This is a noun
-                #textblob detecting 'i' as a noun
-                noun = w
-                break
-    return noun
-
-
-def findAdjective(sent):
-    '''Return adjective represended as JJ in NLTK'''
-    adj = None
-    for w, p in sent.pos_tags:
-        if p == 'JJ':  # This is an adjective
-            adj = w
-            break
-    return adj
-
-
-def startsWithVowel(word):
-    '''Used to check if a noun starts with a vowel
-    then we can assign the proper pronoun'''
-    return True if word[0] in 'aeiou' else False
+	'''Delete any data from the user session log'''
+	chat_log[senderId]['times_contacted'] = 0
+	chat_log[senderId]['drinks_served'] = 0
 
 
 #all code above this created API
@@ -238,11 +121,11 @@ app = Flask(__name__) #create the app server to recieve json
 
 @app.route('/givenMessage', methods = ['POST'])
 def postJsonHandler():
-    '''Receives POST request from webhook and returns POST data'''
-    #print (request.is_json)
-    content = request.get_json()
-    #print (content)
-    message = createMessage(content)
-    return message
+	'''Receives POST request from webhook and returns POST data'''
+	#print (request.is_json)
+	content = request.get_json()
+	#print (content)
+	message = createMessage(content)
+	return message
 
 app.run(host = '0.0.0.0', port = 8090)
